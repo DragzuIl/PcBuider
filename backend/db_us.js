e24754e2369756f45ca661652d06cc20aac734a2
DragzuIl <Kostyamobila@gmail.com>
2025-12-16 19:27:47 +0700

    addcards

---
+++ backend/db_us.js
@@ -0,0 +1,25 @@
+const sql = require('mssql');
+
+const config = {
+  user: 'db_user',
+  password: 'db_pass',
+  server: 'localhost',
+  database: 'PcBuilder',
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+};
+
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log('Connected to MSSQL');
+    return pool;
+  })
+  .catch(err => console.log('DB Connection Failed: ', err));
+
+module.exports = {
+  sql,
+  poolPromise
+};
